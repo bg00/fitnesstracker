325d525c12b5492c2d23f912473c1ead2c28ff12
Bailey Garrett <baileyagarrett@gmail.com>
2020-03-11 17:39:03 -0500

    trying to figure out why API route does not work in api.js and why ncaught (in promise) SyntaxError: Unexpected token < in JSON at position 0 happens in exercise.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker"
     useFindAndModify: false
 });
 
-app.use(require("./public"));
+app.use(express.static("public"));
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
